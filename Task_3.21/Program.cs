2c438ec2212d41bdeea41e5a7ec343fef97bdf3c
Orochi <orochi705@gmail.ru>
2022-05-03 15:49:10 +0900

    push

---
+++ Task_3.21/Program.cs
@@ -0,0 +1,4 @@
+﻿int x1 = 7, y1 = -5, z1 = 0, x2 = 1, y2 = -1, z2 = 9;
+double distance;
+distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
