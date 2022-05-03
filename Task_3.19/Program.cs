3b18ee55a5076b01ffd5533792829947d8989842
Orochi <orochi705@gmail.ru>
2022-05-03 15:47:17 +0900

    push

---
+++ Task_3.19/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = 14212;
+int a = int.Parse(Convert.ToString(number)[0].ToString());
+int b = int.Parse(Convert.ToString(number)[4].ToString());
+int c = int.Parse(Convert.ToString(number)[1].ToString());
+int d = int.Parse(Convert.ToString(number)[3].ToString());
+
+if ((a == b) | (c == d))
+{System.Console.WriteLine("Да, палиндромом ");}
+else {System.Console.WriteLine("Нет");}
